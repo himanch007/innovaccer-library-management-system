8197d3a2d22daab034a0f1b814efd4af87eabd98
Himanch Chowdary <himanchchowdary@gmail.com>
2021-12-07 15:18:56 +0530

    initial setup

---
+++ model.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+u_id = 1
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:@localhost/library"
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.String(100), primary_key = True, unique = True)
+    name = db.Column(db.String(100), unique=False, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(250), unique = False, nullable=False)
+
+
