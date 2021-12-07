158e02082329d3e45974e89cccb8592a6114c33a
Himanch Chowdary <himanchchowdary@gmail.com>
2021-12-07 23:25:41 +0530

    feat: add user registration

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, app, request
 from flask.templating import render_template
 from models import *
+from sqlalchemy import create_engine
+
+u_id = 1
+b_id = 1
 
 #Create an index page
 @app.route('/')
@@ -12,8 +16,8 @@ def indexPage():
 def userLogin():
     return render_template('login.html')
 
-
-@app.route('/success')
+#successful login
+@app.route('/success-login')
 def success():
     if request.method == 'POST':
         email = request.form.get('username')
@@ -28,25 +32,30 @@ def success():
     data = "Wrong Password"
     return render_template('success.html', data = data)
 
-
 #creating register page
 @app.route('/register/')
 def registerPage():
+
     return render_template('register.html')
 
-@app.route('/registersuccess', methods=["POST"])
+#successful register
+@app.route('/success-register', methods=["POST"])
 def registerSuccess():
+    result = 1
     if request.method == "POST":
-        # data = request.form.to_dict()
-        # name = data['name']
         name = request.form.get('name')
         email = request.form.get('email')
         password = request.form.get('password')
-        entry = User(name=name,email=email,password=password)
+        # db_string = "postgresql://postgres:root@127.0.0.1:5432/library"
+        # db = create_engine(db_string)
+        # if("db.session.query(User).filter(count(id))" == 0):
+        #     result = db.execute("db.session.query(User).filter(max(id))") + 1
+        entry = Users(name=name,email=email,password=password)
         db.session.add(entry)
         db.session.commit()
-    #return render_template('success.html', form_data=data)
+        #u_id += 1
     return render_template('login.html')
+
 
 # @app.route('/loginsuccess', methods=['POST'])
 # def loginSucess():
@@ -62,6 +71,22 @@ def registerSuccess():
 #             # print("Name: ",row.name, "Email: ",row.email, "Password: ", row.password)
 #     data = "Wrong Password"
 #     return render_template('login.html', data = data)
+
+
+@app.route('/loginsuccess', methods=['POST'])
+def loginSucess():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        #result = db.session.query(User).all()
+        result = db.session.query(User).filter(User.email==email, User.password==password)
+        for row in result:
+            if (len(row.email)!=0):
+                print("Welcome ",row.name)
+                return render_template('dashboard.html', data=row.name)
+            # print("Name: ",row.name, "Email: ",row.email, "Password: ", row.password)
+    data = "Wrong Password"
+    return render_template('login.html', data = data)
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -1,21 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-u_id = 1
-b_id = 1
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:@localhost/library"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:root@localhost/library"
 db = SQLAlchemy(app)
 
 
 class Users(db.Model):
-    id = db.Column(db.Integer, primary_key = True, unique = True)
+    id = db.Column(db.Integer, primary_key = True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(250), unique = False, nullable=False)
 
 class Books(db.Model):
-    id = db.Column(db.Integer, primary_key = True, unique = True)
+    id = db.Column(db.Integer, primary_key = True, autoincrement=True)
     name = db.Column(db.String(100), unique=False, nullable=False)
     category = db.Column(db.String(100), nullable=False)
     author = db.Column(db.String(100), nullable=False)
